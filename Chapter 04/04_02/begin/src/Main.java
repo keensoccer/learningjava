4e461bf2bd7269e9eb03dda7d4cb05005544f7a0
Achintya G <achintya.9upt@gmail.com>
2018-08-19 15:32:32 -0700

    Finished

--- Chapter 04/04_02/begin/src/Main.java
+++ Chapter 04/04_02/begin/src/Main.java
@@ -4,6 +4,15 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        BankAccount myBankAccount = new BankAccount(4141,
+                100);
+        // System.out.println(myBankAccount.bank_account);
+
+        myBankAccount.deposit(-60);
+        myBankAccount.withdraw(150);
+
+        myBankAccount.deposit(100);
+        myBankAccount.withdraw(200);
 
     }
 
